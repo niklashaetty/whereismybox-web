f5f9fcbde23de867adfc5fb5aa98713544ccd80c
Niklas Hätty <niklas.hatty@invidem.com>
2023-06-06 15:32:03 +0200

    Fix

--- whereismybox-web/api/Functions/HttpTriggers/V2/GetUserByCollectionIdV2Function.cs
+++ whereismybox-web/api/Functions/HttpTriggers/V2/GetUserByCollectionIdV2Function.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Functions.HttpTriggers.V2;
@@ -23,10 +24,13 @@ public class GetUserByCollectionIdV2Function
     private const string OperationId = "GetUserByCollectionIdV2";
     private const string FunctionName = OperationId + "Function";
     private readonly IQueryHandler<GetUserByCollectionIdQuery, User> _queryHandler;
+    private readonly ILogger _logger;
 
-    public GetUserByCollectionIdV2Function(IQueryHandler<GetUserByCollectionIdQuery, User> queryHandler)
+    public GetUserByCollectionIdV2Function(ILoggerFactory loggerFactory, IQueryHandler<GetUserByCollectionIdQuery, User> queryHandler)
     {
+        ArgumentNullException.ThrowIfNull(loggerFactory);
         ArgumentNullException.ThrowIfNull(queryHandler);
+        _logger = loggerFactory.CreateLogger<GetUserByCollectionIdV2Function>();
         _queryHandler = queryHandler;
     }
 
@@ -42,6 +46,9 @@ public class GetUserByCollectionIdV2Function
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users")] 
         HttpRequest req)
     {
+        var bearer = req.Headers["Authorization"];
+        _logger.LogInformation("[AuthHeader] : {AuthHeader}", bearer);
+        
         if (CollectionId.TryParse(req.Query["primaryCollectionId"], out var primaryCollectionId) is false)
         {
             return new BadRequestObjectResult(new ErrorResponse("Validation error", "Invalid primaryCollectionId"));
