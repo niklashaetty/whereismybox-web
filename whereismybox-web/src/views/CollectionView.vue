40cf291d521b76626afdce5841625e2dcdad025d
Niklas Hätty <niklas.hatty@invidem.com>
2023-09-24 21:52:10 +0200

    fed

--- whereismybox-web/src/views/CollectionView.vue
+++ whereismybox-web/src/views/CollectionView.vue
@@ -24,7 +24,7 @@ import type Contributor from '@/models/Contributor';
 import { useLoggedInUserStore } from '@/stores/loggedinuser'
 import { usePaperizer } from 'paperizer'
 import { useToast } from "primevue/usetoast";
-import {  AxiosError } from 'axios'
+import type { AxiosError } from 'axios';
 
 const boxes = ref<Box[]>([]);
 const contributors = ref<Contributor[]>([]);
